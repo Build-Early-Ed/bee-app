bea8ae9e7e59ef4c4097760f84ce74a6080e91dd
Alija <alijablack@fastmail.com>
2021-03-17 15:37:09 -0600

    About Update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="about">
 <section>
-      <b-container>
+      <div class=container>
             <H3>Our Mission</H3>
             <p>Build Early Ed strengthens the continuum of care for youth by connecting New Mexicans to
                 Head Start programs. We help New Mexicans enroll their children into Head Start, earn higher
                 education credentials, and connect with local advocates to build Head Start programs in their
                 community. Together, we can build the future of early childhood education.
             </p>
-      </b-container>
+      </div>
 </section>
 
 </div>
